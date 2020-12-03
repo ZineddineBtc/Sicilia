df9a90bf1c95d490855cc1dd39726e2d41cf84fd
Zineddine Bettouche <zineddine.bettouche@stud.th-deg.de>
2020-12-03 23:00:04 +0100

    Display initial pawn options implemented

--- app/src/main/java/com/example/sicilia/MainActivity.java
+++ app/src/main/java/com/example/sicilia/MainActivity.java
@@ -5,21 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.Gravity;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridLayout;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     DisplayMetrics displayMetrics;
     GridLayout mainGL;
     LinearLayout[] cells = new LinearLayout[64];
-    TextView[] pieces = new TextView[32];
+    TextView[] pieces = new TextView[64];
     LinearLayout.LayoutParams cellsParams, piecesParams;
-    String[] piecesNotation = {"R", "N", "B", "K", "Q", "B", "N", "R"};
-    boolean a = false;
-
+    String rookW="Rw", knightW="Nw", bishopW="Bw", queenW="Qw", kingW="Kw", pawnW="pw",
+           rookB="Rb", knightB="Nb", bishopB="Bb", queenB="Qb", kingB="Kb", pawnB="pb",
+           empty="";
+    String[] piecesInitialPositionW =
+            {rookW, knightW, bishopW, queenW, kingW, bishopW,knightW, rookW};
+    String[] piecesInitialPositionB =
+            {rookB, knightB, bishopB, queenB, kingB, bishopB, knightB, rookB};
+    String[][] board = new String[8][8];
+    int count=0, lastClickedIndex;
+    boolean a=false, optionShown=false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         initializeMainGL();
         fillGrid();
-
+        setBoard();
     }
     private void initializeMainGL(){
         displayMetrics = new DisplayMetrics();
@@ -37,58 +46,114 @@ public class MainActivity extends AppCompatActivity {
         gridParams.height = displayMetrics.widthPixels;
         mainGL.setLayoutParams(gridParams);
     }
-    private void setCellsParams(int i){
+    private void setCellsParams(final int i){
         cells[i] = new LinearLayout(this);
-        cells[i].setBackgroundColor(getColor(a?R.color.colorAccent:R.color.colorPrimary));
+        cells[i].setBackgroundColor(getColor(a?R.color.black_cell:R.color.white_cell));
         cellsParams = new LinearLayout.LayoutParams(displayMetrics.widthPixels/9,
                         displayMetrics.widthPixels/9);
         cellsParams.setMargins(2, 2, 2, 2);
+        cells[i].setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myOnClick(i);
+            }
+        });
     }
     private void setPiecesParams(){
         piecesParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
                 ViewGroup.LayoutParams.MATCH_PARENT);
         piecesParams.setMargins(2, 2, 2, 2);
     }
-    private void appendBlackPieces(int i){
+    private void initializePiece(int i){
         pieces[i] = new TextView(this);
-        pieces[i].setTextColor(getColor(R.color.black));
         pieces[i].setGravity(Gravity.CENTER);
         pieces[i].setTextSize(20);
-        if(i<8) {
-            pieces[i].setText(piecesNotation[i]);
-        }else{
-            pieces[i].setText("p");
-        }
-        cells[i].addView(pieces[i], piecesParams);
     }
-    private void appendWhitePieces(int i){
-        pieces[i-32] = new TextView(this);
-        pieces[i-32].setTextColor(getColor(R.color.white));
-        pieces[i-32].setGravity(Gravity.CENTER);
-        pieces[i-32].setTextSize(20);
-        if(i<56){
-            pieces[i-32].setText("p");
+    private void setBlackPiece(int i){
+        pieces[i].setTextColor(getColor(R.color.black));
+        if(i<8) {
+            pieces[i].setText(piecesInitialPositionB[i]);
         }else{
-            pieces[i-32].setText(piecesNotation[i-56]);
+            pieces[i].setText(pawnB);
         }
-        cells[i].addView(pieces[i-32], piecesParams);
+    }
+    private void setWhitePiece(int i){
+        pieces[i].setTextColor(getColor(R.color.white));
+        if(i<56){
+            pieces[i].setText(pawnW);
+        }else{
+            pieces[i].setText(piecesInitialPositionW[i-56]);
+        }
     }
     private void fillGrid(){
         for(int i = 0; i < 64; i++){
             setCellsParams(i);
+            initializePiece(i);
             if(i<16 || i>=48) {
                 setPiecesParams();
                 if (i < 16) {
-                    appendBlackPieces(i);
+                    setBlackPiece(i);
                 } else {
-                    appendWhitePieces(i);
+                    setWhitePiece(i);
                 }
+            }else{
+                pieces[i].setText("");
             }
+            cells[i].addView(pieces[i], piecesParams);
             mainGL.addView(cells[i], i, cellsParams);
             if((i+1)%8!=0) a = !a;
         }
     }
+    private void setBoard(){
+        for(int i=0; i<8; i++){
+            for(int j=0; j<8; j++){
+                if(i==0){
+                    board[i][j] = piecesInitialPositionB[j];
+                }else if(i==1){
+                    board[i][j] = pawnB;
+                }else if(i==6){
+                    board[i][j] = pawnW;
+                }else if(i==7){
+                    board[i][j] = piecesInitialPositionW[j];
+                }else{
+                    board[i][j] = empty;
+                }
+            }
+        }
+    }
+    private void myOnClick(int i){
+        int row = i/8;
+        int column = i%8;
+        String piece = board[row][column];
+        //Toast.makeText(getApplicationContext(), piece, Toast.LENGTH_LONG).show();
+        if(optionShown) {
+            resetBackground();
+        }
+        if(piece.equals(pawnB) || piece.equals(pawnW)){
+            pawnOptions(row, column, i);
+        }
 
+    }
+    private void pawnOptions(int row, int column, int i){
+        optionShown = true;
+        if(count==0){
+            if(row==1) {
+                cells[i + 8].setBackgroundColor(getColor(R.color.grey));
+                cells[i + 16].setBackgroundColor(getColor(R.color.grey));
+            }else{
+                cells[i - 8].setBackgroundColor(getColor(R.color.grey));
+                cells[i - 16].setBackgroundColor(getColor(R.color.grey));
+            }
+        }
+    }
+    private void resetBackground(){
+        boolean toggle = false;
+        for(int i=0; i<64; i++){
+            cells[i].setBackgroundColor(getColor(toggle?R.color.black_cell:R.color.white_cell));
+            if((i+1)%8!=0) toggle = !toggle;
+        }
+        optionShown = false;
+    }
 }
 
 
